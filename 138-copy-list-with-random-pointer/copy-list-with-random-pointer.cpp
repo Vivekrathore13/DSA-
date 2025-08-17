ab840480670c5074e667b33f0aa28ec9563a86f6
Vivekrathore13 <148472573+Vivekrathore13@users.noreply.github.com>
2025-08-17 15:07:30 +0530

    Time: 5 ms (65.41%) | Memory: 14.8 MB (99.41%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,37 +17,67 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-    // STEP 1 -> CREATE THE DEEP COPY WITHOUT RANDOM POINTER
-    Node* dummy=new Node(0);
-    Node* tempC=dummy;
+    
+    // here is 4 step solution of this code
+
+    //step 1 deep copy
+
+    // deep copy of same linked list using extra dummy node
+    Node* dummy= new Node(0);
+    Node* tempd=dummy;
     Node* temp=head;
-    while(temp){
-        Node* a = new Node(temp->val);
-        tempC->next=a;
-        tempC=tempC->next;
-        temp=temp->next;
+    while(temp!=NULL){
+        Node* a= new Node(temp->val);
+        tempd->next=a;
+        tempd=tempd->next;
+        temp=temp->next; 
     }
-    Node* b = dummy->next;
-    Node* a = head;
-    //STEP 2 MAKE A MAP OF ORIGINAL AND DUPLICATE
-    unordered_map<Node*,Node*> m;
-    Node* tempa = a;
-    Node* tempb = b;
-    while(tempa!=NULL){
-        m[tempa]=tempb;
-        tempa=tempa->next;
-        tempb=tempb->next;
+
+    //Step 2 Making alternate connection using extra dummy node 
+
+    Node* dupli=dummy->next;
+    Node* a=head;
+    Node* b=dupli;
+    dummy=new Node(-1);// initilize again dummy -1
+    Node* tdn=dummy;// it is new temp of dummy node wgich is initilize again with -1
+    //main logic of alterante connection
+    while(a){
+       tdn->next=a;
+       a=a->next;
+       tdn=tdn->next;
+       tdn->next=b;
+       b=b->next;
+       tdn=tdn->next;
     }
-    for(auto x : m){
-        Node* o=x.first;
-        Node* d=x.second;
-        if(o->random!=NULL){
-            //kaam karo
-            Node* orandom= o->random;
-            Node* drandom= m[o->random];
-            d->random= drandom;
-        }
+    dummy=dummy->next;
+
+    //Making random connection importent step
+    Node* t1=dummy;// t1 traverse the original list 
+    while(t1){
+        Node* t2=t1->next; // t2 is traverse for duplicate list
+        if(t1->random) t2->random=t1->random->next; // making random connection
+        t1=t1->next->next;
     }
-    return b;
- }
+
+    // step 4 seperate the list //removing the connection
+    //make the two new node to bulid coonection 
+    Node* d1=new Node(0);
+    Node* d2=new Node(0);
+    t1=d1;
+    Node* t2=d2;
+    Node* t=dummy;
+    while(t){
+        t1->next=t;
+        t=t->next;
+        t1=t1->next;
+        t2->next=t;
+        t=t->next;
+        t2=t2->next;
+    }
+    t1->next=NULL;
+    t2->next=NULL;
+    d1=d1->next; //original with random 
+    d2=d2->next;// duplicate with random
+    return d2;
+    }
 };
