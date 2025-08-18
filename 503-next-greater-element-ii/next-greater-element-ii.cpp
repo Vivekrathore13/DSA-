adba8243e81b4357b3b0ddea78769c5a2f9c5eb3
Vivekrathore13 <148472573+Vivekrathore13@users.noreply.github.com>
2025-08-18 22:48:32 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (37.46%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,6 +4,8 @@ public:
     int n=nums.size();
     vector<int> ans(n,0);
     stack<int> st;
+    // to solve this question think like same array is present then 
+    // to map the element use i%n and loop start with 2*n-1
     for(int i=2*n-1;i>=0;i--){
         while(st.size()>0 && nums[st.top()]<=nums[i%n]){
             st.pop();
