6b481f5800fc2ebf1cefd8edd27c4801140867bd
Vivekrathore13 <148472573+Vivekrathore13@users.noreply.github.com>
2025-08-18 15:56:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (38.80%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // Step 1: copy nums2 elements into nums1 starting from index m
+        
         for (int i = 0; i < n; i++) {
             nums1[m + i] = nums2[i];
         }
