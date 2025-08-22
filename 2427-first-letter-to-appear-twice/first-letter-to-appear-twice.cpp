f8780b71abff0afa1c275b6437eb783646a2dd55
Vivekrathore13 <148472573+Vivekrathore13@users.noreply.github.com>
2025-08-22 23:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (69.42%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_map<char,int> mp;
+
+        for(char c : s) {
+            mp[c]++;
+            if(mp[c] == 2) {   // as soon as frequency reaches 2, return
+                return c;
+            }
+        }
+        
+        return ' '; // fallback (should never hit in valid input)
+    }
+};
