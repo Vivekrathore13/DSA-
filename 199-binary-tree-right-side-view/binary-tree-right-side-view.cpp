d0e44581311da8820619a4a2f95f6b0a498ff3eb
Vivekrathore13 <148472573+Vivekrathore13@users.noreply.github.com>
2025-09-01 16:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (60.94%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -17,24 +17,32 @@ public:
 } 
 
 // Preorder
-void nthlevel(TreeNode* root,int curr,int level,vector<int>& ans){
+// void nthlevel(TreeNode* root,int curr,int level,vector<int>& ans){
+//     if(root==NULL) return;
+//     if(curr==level){
+//         ans[curr]=root->val;
+//         return;
+//     }
+//     nthlevel(root->left,curr+1,level,ans);
+//     nthlevel(root->right,curr+1,level,ans);
+// }
+//     void leveltraversal(TreeNode* root,vector<int>& ans){
+//     int n= ans.size();
+//     for(int i=0;i<n;i++){
+//         nthlevel(root,0,i,ans);
+//     }
+// }
+
+// Another way is too print
+void pre(TreeNode* root,vector<int>& ans,int level){
     if(root==NULL) return;
-    if(curr==level){
-        ans[curr]=root->val;
-        return;
-    }
-    nthlevel(root->left,curr+1,level,ans);
-    nthlevel(root->right,curr+1,level,ans);
-}
-    void leveltraversal(TreeNode* root,vector<int>& ans){
-    int n= ans.size();
-    for(int i=0;i<n;i++){
-        nthlevel(root,0,i,ans);
-    }
+    ans[level]=root->val;
+    pre(root->left,ans,level+1);
+    pre(root->right,ans,level+1);
 }
     vector<int> rightSideView(TreeNode* root) {
     vector<int> ans(level(root),0);
-    leveltraversal(root,ans);
+    pre(root,ans,0);
     return ans;
     }
 };
