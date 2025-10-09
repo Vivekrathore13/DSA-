8eaff0880ecf031eada598bbc750a9d0e0616d6e
Vivekrathore13 <148472573+Vivekrathore13@users.noreply.github.com>
2025-10-10 00:10:20 +0530

    Time: 71 ms (56.90%) | Memory: 14.4 MB (63.40%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    int n;
-    int dp[2501][2501]; // dp[i][p] stores LIS starting from index i with previous index p
-
-    // Recursive function to compute LIS
-    int solve(vector<int>& nums, int i, int p) {
-        if (i >= n) return 0;
-
-        // If already computed
-        if (p != -1 && dp[i][p] != -1) return dp[i][p];
-
-        int take = 0;
-        // Option 1: Take current element (if increasing)
-        if (p == -1 || nums[p] < nums[i]) {
-            take = 1 + solve(nums, i + 1, i);
-        }
-
-        // Option 2: Skip current element
-        int skip = solve(nums, i + 1, p);
-
-        // Store in dp (only when p != -1)
-        if (p != -1)
-            dp[i][p] = max(take, skip);
-
-        return max(take, skip);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(nums, 0, -1);
+    int n=nums.size();
+    vector<int> t(n,1);
+
+    int maxlis=1;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(nums[j]<nums[i]){
+                t[i]=max(t[i],t[j]+1);
+                maxlis=max(maxlis,t[i]);
+            }
+        }
+    }  
+    return maxlis;
     }
 };
