40397e19d60372576f3cf4a30f56dada7bd8b743
Vivekrathore13 <148472573+Vivekrathore13@users.noreply.github.com>
2025-10-11 21:46:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.88%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+    int n=nums.size();
+    int noz=0;
+    int noo=0;
+    int no2=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]==0) noz++;
+        else if(nums[i]==1) noo++;
+        else no2++;
+    }    
+    for(int i=0;i<n;i++){
+        if(i<noz) nums[i]=0;
+        else if(i<(noz+noo)) nums[i]=1;
+        else nums[i]=2;
+    }
+    }
+};
