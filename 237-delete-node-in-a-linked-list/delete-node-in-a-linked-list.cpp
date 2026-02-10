20004d611a2355ad5869e50f02ff1cc8a4022266
Vivekrathore13 <148472573+Vivekrathore13@users.noreply.github.com>
2026-02-10 23:24:56 +0530

    Time: 7 ms (53.03%) | Memory: 12.5 MB (20.87%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -8,8 +8,15 @@
  */
 class Solution {
 public:
-    void deleteNode(ListNode* target) {
-    target->val=target->next->val;
-    target->next=target->next->next ;   
+    void deleteNode(ListNode* tar) {
+    // in this question is tricky because we are not deleting the ndoe delete the value so the it becomes
+
+     // 10,20 ,30 ,40 ,50 =>tar =30 
+     // so tho 30 ko 40 bana do dur 40 ko 50 bus 
+
+     // so the code is 
+
+     tar->val=tar->next->val;
+     tar->next=tar->next->next; 
     }
 };
